d5b57189aced1921756181c8326c3b49cb42a8e4
emo20142email.vccs.edu <emo2014@email.vccs.edu>
2026-03-22 22:46:50 -0400

    EDIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import "./index.css"
 
-import Amsterdamroof from "./assets/Amsterdam roof.jpg"
-import emshibiscus from "./assets/ems hibiscus.JPG"
+import hibiscus from "./assets/hibiscus.jpg"
+import amsterdamRoof from "./assets/amsterdam-roof.jpg"
 import motions from "./assets/motions.png"
-import redlightdistrict from "./assets/RED LIGHT DISCTRICT.png"
-import Glasgowrainynight from "./assets/Rain on a cheerful night. Glasgow, UK.JPG"
-import pinkcityamsterdam from "./assets/pink city, Amsterdam, NL.jpg"
-import Cambridgeriver from "./assets/Cambridge, UK.JPEG"
-import oldTownMoers from "./assets/moers city walk.jpg"
-import windowtogermany from "./assets/life through glass, Moers, Germany.png"
-  
+import redLightDistrict from "./assets/red-light-district.png"
+import glasgowNight from "./assets/glasgow-rainy-night.jpg"
+import pinkAmsterdam from "./assets/pink-city-amsterdam.jpg"
+import cambridgeRiver from "./assets/cambridge-river.jpeg"
+import moersWalk from "./assets/moers-city-walk.jpg"
+import lifeThroughGlass from "./assets/lifethroughglass-moers-germany.png"
+
 function App() {
 return ( <div>
 
 ```
   <nav className="nav">
     <h2>Emily.dev</h2>
-   </nav>
+  </nav>
 
-   <section id="creative" className="section">
+  <section className="hero">
+    <img src={hibiscus} className="hero-img" />
+
+    <div className="hero-text">
+      <h1>Emily — Frontend Developer</h1>
+      <p>
+        I build modern user-focused web experiences with strong visual
+        awareness, creativity, and attention to detail.
+      </p>
+    </div>
+  </section>
+
+  <section className="section">
     <h2>Photography</h2>
 
     <div className="gallery">
-      <img src={Amsterdamroof} />
-      <img src={emshibiscus} />
+      <img src={amsterdamRoof} />
       <img src={motions} />
-      <img src={redlightdistrict} />
-      <img src={Glasgowrainynight} />
-      <img src={pinkcityamsterdam} />
-      <img src={Cambridgeriver} />
-      <img src={oldTownMoers} />
-      <img src={windowtogermany} />
+      <img src={redLightDistrict} />
+      <img src={glasgowNight} />
+      <img src={pinkAmsterdam} />
+      <img src={cambridgeRiver} />
+      <img src={moersWalk} />
+      <img src={lifeThroughGlass} />
     </div>
- 
+
   </section>
 
 </div>
